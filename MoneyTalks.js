220409dd3cbc28cf57f6a77064e9bba1920d5231
Stefano Balietti <futur.dorko@gmail.com>
2012-08-12 19:49:56 +0200

    fixed bug in MoneyTalks

---
+++ MoneyTalks.js
@@ -0,0 +1,70 @@
+(function (exports) {
+	
+	exports.MoneyTalks	= MoneyTalks;
+	
+	JSUS = node.JSUS;
+	
+	MoneyTalks.id = 'moneytalks';
+	MoneyTalks.name = 'Money talks';
+	MoneyTalks.version = '0.1.0';
+	MoneyTalks.description = 'Display the earnings of a player.';
+	
+	MoneyTalks.dependencies = {
+		JSUS: {},
+	};
+	
+	
+	function MoneyTalks (options) {
+		this.id = MoneyTalks.id;
+		
+		this.fieldset = {legend: 'Earnings'};
+		
+		this.root = null;		// the parent element
+		
+		this.spanCurrency = document.createElement('span');
+		this.spanMoney = document.createElement('span');
+		
+		this.currency = 'EUR';
+		this.money = 0;
+		this.precision = 2;
+		this.init(options);
+	}
+	
+	
+	MoneyTalks.prototype.init = function (options) {
+		this.currency = options.currency || this.currency;
+		this.money = options.money || this.money;
+		this.precision = options.precision || this.precision;
+		
+		this.spanCurrency.id = options.idCurrency || this.spanCurrency.id;
+		this.spanMoney.id = options.idMoney || this.spanMoney.id;
+		
+		this.spanCurrency.innerHTML = this.currency;
+		this.spanMoney.innerHTML = this.money;
+	};
+	
+	MoneyTalks.prototype.getRoot = function () {
+		return this.root;
+	};
+	
+	MoneyTalks.prototype.append = function (root, ids) {
+		var PREF = this.id + '_';
+		root.appendChild(this.spanMoney);
+		root.appendChild(this.spanCurrency);
+		return root;
+	};
+		
+	MoneyTalks.prototype.listeners = function () {
+		var that = this;
+		node.on('MONEYTALKS', function(amount) {
+			that.update(amount);
+		}); 
+	};
+	
+	MoneyTalks.prototype.update = function (amount) {
+		if ('number' !== typeof amount) return;
+		this.money += amount;
+		this.spanMoney.innerHTML = this.money.toFixed(this.precision);
+	};
+	
+})(node.window.widgets);
