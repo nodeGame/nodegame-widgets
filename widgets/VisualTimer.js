f154710af1a514ad2bea0bfbb95c85cdd2ef1823
lmis <lmiserez@student.ethz.ch>
2014-07-31 12:47:14 +0200

    Added Documentation to VisualTimer.js;

--- widgets/VisualTimer.js
+++ widgets/VisualTimer.js
@@ -38,18 +38,44 @@
         this.options.update = ('undefined' === typeof this.options.update) ?
             1000 : this.options.update;
 
-        this.id = options.id;
-
+        /**
+         *  ### gameTimer
+         *  
+         *  The timer which counts down the game time.
+         *
+         *  @see node.timer.createTimer  
+         */
         this.gameTimer = null;
         
-        // The DIV in which to display the timer.
+        /**
+         *  ### timerDiv
+         *  The DIV in which to display the time left to make a move.
+         */
         this.timerDiv = null;   
         
-        // The DIV in which to display the maximum waiting time left.
+        /**
+         *  ### waitDiv
+         *  The DIV in which to display the maximum waiting time left. 
+         */
         this.waitDiv = null;
         
+        /**
+         *  ### runDiv
+         *  The DIV in which to display the time.
+         *  
+         *  This variable is always a reference to either 'waitDiv' or 
+         *  'timerDiv'. 
+         */
         this.runDiv = null;
         
+        /**
+         *  ### timeLeft
+         *  Stores the time left when 'stop' is called.
+         *  
+         *  Not to be confused with gameTimer.timeLeft
+         *
+         *  @see VisualTimer.stop
+         */
         this.timeLeft = null;
 
         this.init(this.options);
@@ -92,7 +118,7 @@
             get: function() {
                 return {
                     startPaused: t.startPaused,
-	            status: t.status,
+	                status: t.status,
                     timeLeft: t.timeLeft,
                     timePassed: t.timePassed,
                     update: t.update,
@@ -105,6 +131,7 @@
         this.options = options;
         
         this.runDiv = this.timerDiv;
+        
         if (this.waitDiv) {
             this.waitDiv.style.display = 'none';
         }
@@ -131,7 +158,10 @@
         this.runDiv = this.timerDiv;
         this.updateDisplay();
     };
-
+    /**
+     *  ## VisualTimer.updateDisplay
+     *  Changes 'runDiv' to display current time of 'gameTimer'
+     */
     VisualTimer.prototype.updateDisplay = function() {
         var time, minutes, seconds;
         if (!this.gameTimer.milliseconds || this.gameTimer.milliseconds === 0) {
@@ -145,6 +175,16 @@
         this.runDiv.innerHTML = minutes + ':' + seconds;
     };
 
+    /**
+     *  ## VisualTimer.start
+     *  Starts the timer and changes the display accordingly.
+     *
+     *  Starts the 'gameTimer', hides 'waitDiv', unstrikes 'timerDiv' and
+     *  sets 'runDiv' as a reference to 'timerDiv'.
+     *
+     *  @see VisualTimer.updateDisplay
+     *  @see GameTimer.start
+     */
     VisualTimer.prototype.start = function() {
         this.timerDiv.className = '';
         this.runDiv = this.timerDiv;
@@ -153,17 +193,53 @@
         this.gameTimer.start();
     };
 
+    /**
+     *  ## VisualTimer.restart
+     *  Restarts the timer with new options
+     *
+     *  @param {object} options Configuration object
+     *
+     *  @see VisualTimer.init
+     *  @see VisualTimer.start
+     */
     VisualTimer.prototype.restart = function(options) {
         this.init(options);
         this.start();
     };
 
+    /**
+     *  ## VisualTimer.stop
+     *  Stops the timer display and start displaying max. wait time.
+     *
+     *  Does nothing if 'gameTimer' is stopped.
+     *  Otherwise it updates 'timeLeft' with the current time in 'gameTimer',
+     *  and changes the display according to the options object as follows.
+     *
+     *  If 'options.waitTime' is a _negative_ value, the 'gameTimer' is stopped,
+     *  'VisualTimer.updateDisplay' is called and the function is returned
+     *  If 'options' or 'options.waitTime' is _undefined_, the gameTimer is 
+     *  restarted with the current time left on the clock. 
+     *  Uf 'options.waitTime' is a _positive_ value, then the 'gameTimer' is 
+     *  restarted with that value. 
+     *  After the gameTimer has been restarted, 'waitDiv' is unhidden and 
+     *  'runDiv' is set such that 'VisualTimer.updateDisplay' updates 'waitDiv',
+     *  displaying the max. wait time.
+     *
+     *  @param {object} options Configuration object
+     *
+     *  @see VisualTimer.updateDisplay
+     *  @see GameTimer.isStopped
+     *  @see GameTimer.restart
+     *  @see GameTimer.stop
+     */
     VisualTimer.prototype.stop = function(options) {
+        var waitTime;
         if (!this.gameTimer.isStopped()) {
-            var waitTime;
-            this.timeLeft = this.gameTimer.milliseconds - this.gameTimer.timePassed;
+            this.timeLeft = this.gameTimer.milliseconds -
+                    this.gameTimer.timePassed;
             
-            if (typeof options === 'undefined' || typeof options.waitTime === 'undefined') {
+            if (typeof options === 'undefined' ||
+                    typeof options.waitTime === 'undefined') {
                 waitTime = this.timeLeft;
             }
             else {
@@ -171,32 +247,47 @@
             }
             if (waitTime >= 0) {
                 this.gameTimer.restart({milliseconds : waitTime});
-                this.runDiv = this.waitDiv.children[1];
+                this.runDiv = this.waitDiv.children[1]; // timeWaitDiv
                 this.waitDiv.style.display = '';
             }
             else {
                 this.gameTimer.stop();
             }
             this.updateDisplay();
-        }
-            
+        }  
     };
 
-    VisualTimer.prototype.resume = function(options) {
+    /**
+     *  ## VisualTimer.resume
+     *  Resumes the 'gameTimer' and hides 'waitDiv'
+     *
+     *  @see GameTimer.resume
+     */
+    VisualTimer.prototype.resume = function() {
         this.gameTimer.resume();
         this.waitDiv.style.display = 'none';
-        
     };
 
+    /**
+     *  ## VisualTimer.resume
+     *  Stops 'VisualTimer', hides 'waitDiv' and sets 'timerDiv' to zero.
+     *
+     *  @see VisualTimer.stop
+     */
     VisualTimer.prototype.setToZero = function() {
         this.stop();
         this.waitDiv.style.display = 'none';
         this.timerDiv.innerHTML = '00:00';
     };
     
-    VisualTimer.prototype.empty_timer = function(options) {
+    /**
+     *  ## VisualTimer.emptyTimer
+     *  Changes 'timerDiv' to have an empty body.
+     */
+    VisualTimer.prototype.clearTimer = function() {
         this.timerDiv.innerHTML = '';
-    }
+    };
+    
     /**
      * ## VisualTimer.doTimeUp
      *
@@ -231,7 +322,6 @@
             that.stop();
             that.timerDiv.className = 'strike';
        });
-
     };
 
     VisualTimer.prototype.destroy = function() {
@@ -286,5 +376,6 @@
         }
         return options;
     }
+    
 
 })(node);
