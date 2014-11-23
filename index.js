1f87491fa21b8ed216c969173645cbca0d7a73a0
Stefano Balietti <futur.dorko@gmail.com>
2014-11-23 14:34:32 +0100

    mods

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-// # nodegame-widgets
-// Collections of nodeGame widgets
+/**
+ * # nodegame-widgets
+ * Copyright(c) 2014 Stefano Balietti
+ * MIT Licensed
+ *
+ * Collections of nodeGame widgets.
+ */
