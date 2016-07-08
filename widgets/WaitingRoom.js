a03087c10a33ce6de1c30b73f2134829e468b120
Stefano Balietti <futur.dorko@gmail.com>
2016-07-08 11:01:42 -0400

    improved WaitRoom.alertPlayers

--- widgets/WaitingRoom.js
+++ widgets/WaitingRoom.js
@@ -517,11 +517,34 @@
     };
 
     WaitingRoom.prototype.alertPlayer = function() {
-        var opts;
+        var clearBlink, onFrame;
+
         JSUS.playSound('/sounds/doorbell.ogg');
-        if (document.hasFocus && document.hasFocus()) opts = { repeatFor: 1 };
-        else opts = { stopOnFocus: true, stopOnClick: window };
-        JSUS.blinkTitle(['3', '2', '1', 'GAME STARTS!'], opts);
+        // If document.hasFocus() returns TRUE, then just one repeat is enough.
+        if (document.hasFocus && document.hasFocus()) {
+            JSUS.blinkTitle('GAME STARTS!', { repeatFor: 1 });
+        }
+        // Otherwise we repeat blinking until an event that shows that the
+        // user is active on the page happens, e.g. focus and click. However,
+        // the iframe is not created yet, and even later. if the user clicks it
+        // it won't be detected in the main window, so we need to handle it.
+        else {
+            clearBlink = JSUS.blinkTitle('GAME STARTS!', {
+                stopOnFocus: true,
+                stopOnClick: window
+            });
+            onFrame = function() {
+                var frame;
+                clearBlink();
+                frame = W.getFrame();
+                if (frame) {
+                    frame.removeEventListener('mouseover', onFrame, false);
+                }
+            };
+            node.events.ng.once('FRAME_GENERATED', function(frame) {
+                frame.addEventListener('mouseover', onFrame, false);
+            });
+        }
     };
 
     WaitingRoom.prototype.destroy = function() {
