9c815013c9c4c5f8c020796fb89007aa48b6b796
Stefano Balietti <futur.dorko@gmail.com>
2019-03-01 15:16:34 -0500

    fixed lastAppended destroy bug; removed defaults mixout (not used)

--- build/nodegame-widgets.js
+++ build/nodegame-widgets.js
@@ -1233,10 +1233,11 @@
                             'dependencies.');
         }
 
+        // NOT USED ANY MORE.
         // Add default properties to the user options.
-        if (WidgetPrototype.defaults) {
-            J.mixout(options, J.clone(WidgetPrototype.defaults));
-        }
+        // if (WidgetPrototype.defaults) {
+        //     J.mixout(options, J.clone(WidgetPrototype.defaults));
+        // }
 
         // Create widget.
         widget = new WidgetPrototype(options);
@@ -1376,7 +1377,7 @@
             }
 
             // Remove from lastAppended.
-            if (this.lastAppended.wid === widget.wid) {
+            if (this.lastAppended && this.lastAppended.wid === widget.wid) {
                 node.warn('node.widgets.lastAppended destroyed.');
                 this.lastAppended = null;
             }
@@ -1743,6 +1744,242 @@
     ('undefined' !== typeof window) ? window : module.parent.exports.window,
     ('undefined' !== typeof window) ? window.node : module.parent.exports.node
 );
+
+/**
+ * # BoxSelector
+ * Copyright(c) 2019 Stefano Balietti
+ * MIT Licensed
+ *
+ * Creates a simple box that opens a menu of items to choose from
+ *
+ * www.nodegame.org
+ */
+(function(node) {
+
+    "use strict";
+
+    var NDDB =  node.NDDB;
+
+    node.widgets.register('BoxSelector', BoxSelector);
+
+    // ## Meta-data
+
+    BoxSelector.version = '1.0.0';
+    BoxSelector.description = 'Creates a simple box that opens a menu ' +
+        'of items to choose from.';
+
+    BoxSelector.panel = false;
+    BoxSelector.title = false;
+    BoxSelector.className = 'boxselector';
+
+    // ## Dependencies
+
+    BoxSelector.dependencies = {
+        JSUS: {}
+    };
+
+    /**
+     * ## BoxSelector constructor
+     *
+     * `BoxSelector` is a simple configurable chat
+     *
+     * @see BoxSelector.init
+     */
+    function BoxSelector() {
+
+        /**
+         * ### BoxSelector.button
+         *
+         * The button that if pressed shows the items
+         *
+         * @see BoxSelector.ul
+         */
+        this.button = null;
+        
+        /**
+         * ### BoxSelector.buttonText
+         *
+         * The text on the button
+         *
+         * @see BoxSelector.button
+         */
+        this.buttonText = '';
+
+        /**
+         * ### BoxSelector.items
+         *
+         * List of items to choose from
+         */
+        this.items = [];
+
+        /**
+         * ### BoxSelector.clickCb
+         *
+         * A callback to call when an item from the list is clicked
+         *
+         * @see BoxSelector.items
+         */
+        this.clickCb = null;
+
+        /**
+         * ### BoxSelector.getText
+         *
+         * A callback that renders an element into a text
+         */
+        this.getText = null;
+
+        /**
+         * ### BoxSelector.getId
+         *
+         * A callback that returns the id of an item
+         */
+        this.getId = null;
+
+        /**
+         * ### BoxSelector.ul
+         *
+         * The HTML UL element displaying the list of items
+         *
+         * @see BoxSelector.items
+         */
+        this.ul = null;
+    }
+
+    // ## BoxSelector methods
+
+    /**
+     * ### BoxSelector.init
+     *
+     * Initializes the widget
+     *
+     * @param {object} options Configuration options.
+     */
+    BoxSelector.prototype.init = function(options) {
+        if ('function' !== typeof options.getText) {
+            throw new Error('BoxSelector.init: options.getText must be ' +
+                            'function. Found: ' + options.getText);
+        }
+        this.getText = options.getText;
+
+        if ('function' !== typeof options.getId) {
+            throw new Error('BoxSelector.init: options.getId must be ' +
+                            'function. Found: ' + options.getId);
+        }
+        this.getId = options.getId;
+
+
+        if (options.clickCb) {
+            if ('function' !== typeof options.clickCb) {
+                throw new Error('BoxSelector.init: options.getId must be ' +
+                                'function or undefined. Found: ' +
+                                options.getId);
+            }    
+            this.clickCb = options.clickCb;
+        }        
+    };
+
+
+    BoxSelector.prototype.append = function() {
+        var that, ul, btn, btnGroup, toggled;
+        
+        btnGroup = W.add('div', this.bodyDiv);
+        btnGroup.role = 'group';
+        btnGroup['aria-label'] = 'Select Items';
+        btnGroup.className = 'btn-group dropup';
+        
+        // Here we create the Button holding the treatment.
+        btn = this.button = W.add('button', btnGroup);
+        btn.className = 'btn btn-default btn dropdown-toggle';
+        btn['data-toggle'] = 'dropdown';
+        btn['aria-haspopup'] = 'true';
+        btn['aria-expanded'] = 'false';
+        btn.innerHTML = this.buttonText + '&nbsp;';
+
+        W.add('span', btn, { className: 'caret' });
+        
+        // Here the create the UL of treatments.
+        // It will be populated later.
+        ul = this.ul = W.add('ul', btnGroup);
+        ul.className = 'dropdown-menu';
+        ul.style.display = 'none';
+
+        // Variable toggled controls if the dropdown menu
+        // is displayed (we are not using bootstrap js files)
+        // and we redo the job manually here.
+        toggled = false;
+        btn.onclick = function() {
+            if (toggled) {
+                ul.style.display = 'none';
+                toggled = false;
+            }
+            else {
+                ul.style.display = 'block';
+                toggled = true;
+            }
+        };
+
+        that = this;
+        ul.onclick = function(eventData) {
+            var id, i, len;
+            id = eventData.target;
+            // When '' is hidden by bootstrap class.
+            ul.style.display = '';
+            toggled = false;
+            id = id.parentNode.id;
+            // Clicked on description?
+            if (!id) id = eventData.target.parentNode.parentNode.id;
+            // Nothing relevant clicked (e.g., header).
+            if (!id) return;
+            len = that.items.length;
+            // Call the clickCb.
+            for ( i = 0 ; i < len ; i++) {
+                if (that.getId(that.items[i]) === id) {
+                    that.clickCb(that.items[i], id);
+                    break;
+                }
+            }
+        };
+    };
+
+    BoxSelector.prototype.addItem = function(item) {
+        var ul, li, a, tmp;
+        ul = this.ul;
+        li = document.createElement('li');
+        // Text.
+        tmp = this.getText(item);
+        if (!tmp || 'string' !== typeof tmp) {
+            throw new Error('BoxSelector.addItem: getText did not return a ' +
+                            'string. Found: ' + tmp + '. Item: ' + item);
+        }
+        if (this.clickCb) {
+            a = document.createElement('a');
+            a.href = '#';
+            a.innerHTML = tmp;        
+            li.appendChild(a);
+        }
+        else {
+            li.innerHTML = tmp;        
+        }
+        // Id.
+        tmp = this.getId(item);
+        if (!tmp || 'string' !== typeof tmp) {
+            throw new Error('BoxSelector.addItem: getId did not return a ' +
+                            'string. Found: ' + tmp + '. Item: ' + item);
+        }
+        li.id = tmp;
+        li.className = 'dropdown-header';
+        ul.appendChild(li);
+        this.items.push(item);
+    };
+
+    BoxSelector.prototype.getValues = function() {
+        return this.items;
+    };
+
+    // ## Helper functions.
+
+
+})(node);
 
 /**
  * # Chat
