1d145a0747dab6cac650e0175358851bd5c7075b
Stefano Balietti <futur.dorko@gmail.com>
2021-10-29 13:58:10 +0200

    simplify isCorrect=false

--- widgets/ChoiceManager.js
+++ widgets/ChoiceManager.js
@@ -658,7 +658,11 @@
         if (this.textarea) obj.freetext = this.textarea.value;
 
         // Simplify everything, if requested.
-        if (opts.simplify || this.simplify) obj = obj.forms;
+        if (opts.simplify || this.simplify) {
+            res = obj;
+            obj = obj.forms;
+            if (res.isCorrect === false) obj.isCorrect = false;
+        }
         return obj;
     };
 
